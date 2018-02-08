a7b409d60563d6b2babdae09b8532c4c21d96c32
Arturo Burela <uno@arturoburela.com>
2018-02-08 12:04:52 -0600

    Set project structure

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-var extend = require('extend');
 
 module.exports = {
-  label: 'e-Commerce',
-  extend: 'apostrophe-widgets',
+  name: 'ncc-ecommerce',
+  alias: 'ecommerce',
+  label: 'Ecommerce',
+  extend: 'apostrophe-pieces',
+
+  moogBundle: {
+    modules: ['ncc-ecommerce-pages', 'ncc-ecommerce-widgets'],
+    directory: 'lib/modules'
+  },
 
   beforeConstruct: function(self, options) {
-    options.addFields = [
-      {
-        type: 'string',
-        name: 'account',
-        label: 'Twitter Account'
-        // required: true
-      },
-      {
-        type: 'string',
-        name: 'hashtag',
-        label: 'Filter Tweets by Hashtag'
-      },
-      {
-        type: 'integer',
-        name: 'limit',
-        label: 'Limit Number of Tweets',
-        def: 3
-      }
-    ].concat(options.addFields || []);
+    // Insert code here
   },
 
   construct: function(self, options) {
-    // if (false){//1!options.consumerKey) {
-    //   console.error('WARNING: you must configure the consumerKey, consumerSecret, accessToken and accessTokenSecret options to use the Twitter widget.');
-    // }
-
-    self.getSum = (num1, num2) => num1 + num2;
-
-
-    self.logSelf = () => console.log(JSON.stringify(self));
+    // Insert code here
   }
 };
