e5c36edd8520bb9166bdcd8a17abd8ff88573142
Arturo Burela <uno@arturoburela.com>
2018-03-05 13:54:19 -0600

    Rename module, addPayment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   extend: 'apostrophe-module',
 
   moogBundle: {
-    modules: ['ncc-ecommerce-page', 'ncc-ecommerce-widgets', 'ncc-product', 'ncc-category', 'ncc-subcategory', 'ncc-products-pages', 'ncc-cart', 'ncc-payments'],
+    modules: ['ncc-ecommerce-page', 'ncc-ecommerce-widgets', 'ncc-product', 'ncc-category', 'ncc-subcategory', 'ncc-products-pages', 'ncc-cart', 'ncc-payment'],
     directory: 'lib/modules'
   },
 
