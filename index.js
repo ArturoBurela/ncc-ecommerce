678f2f9be0039406afa633bb34efbb7ed3965f51
Arturo Burela <uno@arturoburela.com>
2018-03-06 12:42:09 -0600

    Change product-payment relations

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   extend: 'apostrophe-module',
 
   moogBundle: {
-    modules: ['ncc-ecommerce-page', 'ncc-ecommerce-widgets', 'ncc-product', 'ncc-category', 'ncc-subcategory', 'ncc-products-pages', 'ncc-cart', 'ncc-payment'],
+    modules: ['ncc-ecommerce-page', 'ncc-ecommerce-widgets', 'ncc-product', 'ncc-category', 'ncc-subcategory', 'ncc-products-pages', 'ncc-cart', 'ncc-payment', 'ncc-product-payment'],
     directory: 'lib/modules'
   },
 
